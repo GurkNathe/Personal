1472c3e46f7e98af9abcd35c10cb32a2adf9a718
GurkNathe <ethan.c.krug@gmail.com>
2023-04-10 22:52:55 -0700

    Added home styling, sidebar issues.

--- personal/src/components/Home.tsx
+++ personal/src/components/Home.tsx
@@ -1,8 +1,9 @@
+import { Suspense, useRef } from "react";
+
 import * as THREE from "three";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { TextureLoader } from "three";
-import { Suspense } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, Text3D } from "@react-three/drei";
+import { Mesh, TextureLoader } from "three";
 
 import "../css/home.css";
 
@@ -11,16 +12,37 @@ const Background = () => {
         "/resources/snowy-mountain.jpg",
         "/resources/plum-blossom-path.jpg",
         "/resources/plum-blossom-temple.jpg",
+        "/resources/mountain-river.jpg",
+        "/resources/desert-ruins.jpg",
+        "/resources/magic-jungle.jpg",
+        "/resources/cyberpunk.jpg",
+        "/resources/temple-peak.jpg",
+        "/resources/future-city.jpg",
+        "/resources/fjords.jpg",
     ]
 
     const bg = useLoader(TextureLoader, bg_array[Math.floor(Math.random() * bg_array.length)]);
+
+    const sphere_mesh = useRef<Mesh>(null!)
+
+    useFrame(({ clock }) => {
+        sphere_mesh.current.rotation.x = Math.sin(clock.getElapsedTime()) * 0.05;
+        sphere_mesh.current.rotation.y = clock.getElapsedTime() * 0.1;
+    })
+
     return(
         <>
-            <ambientLight intensity={0.2} />
-            <directionalLight />
-            <mesh>
-                <sphereGeometry args={[5, 32, 32]}/>
+            <ambientLight intensity={1} />
+            <mesh ref={sphere_mesh}>
+                <sphereGeometry args={[6, 32, 32]}/>
                 <meshStandardMaterial map={bg} side={THREE.BackSide}/>
+                <mesh position={[-3, -0.5, -4]}>
+                    <Text3D font={"/resources/Inter_Regular.json"}>
+                        Ethan Krug
+                        <meshStandardMaterial color="#181818"/>
+                        <directionalLight position={[0,1,1]}/>
+                    </Text3D>
+                </mesh>
             </mesh>
         </>
     )
@@ -31,7 +53,7 @@ export default function Home() {
         <Canvas
             camera={{ fov: 70, near: 0.01, far: 100, position: [0, 0, 1] }}
         >
-            <OrbitControls enableZoom={false}/>
+            <OrbitControls enableZoom={false} rotateSpeed={0.3}/>
             <Suspense fallback={null}>
                 <Background/>
             </Suspense>
