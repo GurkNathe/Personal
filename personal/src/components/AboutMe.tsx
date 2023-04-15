69da0e85493480654c39d3d1ba066f77612ef344
GurkNathe <ethan.c.krug@gmail.com>
2023-04-14 18:49:22 -0700

    Additions About Me.

--- personal/src/components/AboutMe.tsx
+++ personal/src/components/AboutMe.tsx
@@ -12,10 +12,9 @@ export default function AboutMe() {
         nameLoad();
     }, []);
 
-    const nameLoad = (speed: number = 10) => {
+    const nameLoad = (speed: number = 10, original: string = "ETHAN KRUG") => {
         let finalName = "";
         let iterations = 0;
-        const original = "ETHAN KRUG";
         
         const interval = setInterval(() => {
             let newLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -69,7 +68,30 @@ export default function AboutMe() {
                         children={name}
                         onMouseEnter={nameHover}
                     />
-                    <span>test</span>
+                    <span className="intro">
+                        Hello, I'm a computer scientist with a passion for building 
+                        clean and efficient software solutions. I have a degree 
+                        in computer science and a few years of experience in the field, 
+                        working on projects mainly relating to web development 
+                        and data science.<br/><br/>
+
+                        Currently, I am looking for employment. My expertise includes 
+                        proficiency in a variety of programming languages, 
+                        database technologies, and development frameworks.<br/><br/>
+
+                        I enjoy working in a challenging environment where I can freely 
+                        collaborate with other engineers to solve complex problems 
+                        and create software that delivers value to end-users. I am 
+                        committed to staying up-to-date with the latest industry trends 
+                        and technologies to ensure that the software I develop is always 
+                        cutting-edge and meets the highest standards of quality.
+                    </span><br/>
+                    <span className="me-links">
+                        <a href="/resources/Resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+                        <a href="https://github.com/GurkNathe/" target="_blank" rel="noreferrer">GitHub</a>
+                        <a href="https://www.linkedin.com/in/ethan-krug-5a3088171" target="_blank" rel="noreferrer">LinedIn</a>
+                        <a href="mailto:ethan.c.krug@gmail.com" target="_blank" rel="noreferrer">Email</a>
+                    </span>
                 </div>
             </Paper>
         </div>
