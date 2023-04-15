b575392e936a44a1772543a0e76d1e2b47cdfff7
GurkNathe <ethan.c.krug@gmail.com>
2023-04-15 16:46:43 -0700

    Actual article test.

--- articles/csv-json.py
+++ articles/csv-json.py
@@ -8,7 +8,7 @@ def csv_to_json(csvFilePath, jsonFilePath):
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
         for row in csvReader:
-            row["tags"] = row["tags"].strip("[]").split(", ") 
+            row["tags"] = row["tags"].split(", ") 
             if len(row["timestamp"]) == 0:
                 timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
                 row["timestamp"] = timestamp
