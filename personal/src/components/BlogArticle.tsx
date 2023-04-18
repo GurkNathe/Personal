9f092235ff195b4dacdb47ef3fddc79f0b748532
GurkNathe <ethan.c.krug@gmail.com>
2023-04-18 11:37:14 -0700

    Testing article toc.

--- personal/src/components/BlogArticle.tsx
+++ personal/src/components/BlogArticle.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { useLoaderData } from "react-router-dom";
 
 import Paper from "@mui/material/Paper";
@@ -15,17 +17,59 @@ export const articleTextLoader = async (url: string | undefined) => {
     return res;
 };
 
+interface CustomHeaderProps {
+    level: number;
+    children: React.ReactNode[];
+}
+
 export default function BlogArticle() {
     const data = useLoaderData() as string;
+
+    const slug = (text: string) => {
+        return text.toLowerCase().replace(/[^\w]+/g, '-');
+    };
+
+    const CustomHeader: React.FC<CustomHeaderProps> = ({ level, children }) => {
+        const tagName = `h${level}`;
+        const id = slug(children!.toString());
+        return React.createElement(tagName, { id }, children);
+    };
+
+    const updateMarkdownTOCLinks = (markdownText: string) => {
+        const headerRegex = /^#+\s+(.*)$/gm;
+        const tocRegex = /\[TOC\]\((.*)\)/g;
+        
+        const headerMap: any = {};
+        
+        let match;
+        while ((match = headerRegex.exec(markdownText)) !== null) {
+            const headerText = match[1];
+            const headerId = slug(headerText);
+            headerMap[headerText] = headerId;
+        }
+        
+        return markdownText.replace(tocRegex, (match: string, linkText: string) => {
+            const targetId = headerMap[linkText];
+            return targetId ? `[\${linkText}](#\${targetId})` : match;
+        });
+    };
 
     return (
         <div className="page">
             <Paper className="article-paper">
                 <ReactMarkdown
                     className="article"
-                    children={data}
+                    children={updateMarkdownTOCLinks(data)}
                     remarkPlugins={[remarkParse, remarkGfm]}
                     rehypePlugins={[rehypeStringify]}
+                    components={{ 
+                        h1: CustomHeader, 
+                        h2: CustomHeader, 
+                        h3: CustomHeader, 
+                        h4: CustomHeader, 
+                        h5: CustomHeader, 
+                        h6: CustomHeader 
+                    }}
                 />
             </Paper>
         </div>
