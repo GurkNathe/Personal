f381c26654ac4c5a581eb7ccd411ef7c05ecccd5
GurkNathe <ethan.c.krug@gmail.com>
2023-04-29 17:54:22 -0700

    Adding home alt visual.

--- personal/src/App.tsx
+++ personal/src/App.tsx
@@ -3,14 +3,13 @@ import {
     createRoutesFromElements,
     Outlet,
     Route,
-    RouterProvider,
-    useRouteError,
+    RouterProvider
 } from "react-router-dom";
 
 import AboutMe from "./components/AboutMe";
 import BlogArticle, { articleTextLoader } from "./components/BlogArticle";
 import BlogList, { articleLoader } from "./components/BlogList";
-import Home from "./components/Home";
+import Home, { Donut } from "./components/Home";
 import Error404 from "./components/404";
 import SideBar from "./components/SideBar";
 
@@ -26,20 +25,9 @@ export default function App() {
         );
     };
 
-    // TODO: Better visual when no WebGL
-    const ErrorBoundaryHome = () => {
-        const error = useRouteError();
-        console.error("Couldn't load WebGL properly:", error);
-        return (
-            <div style={{display: "flex", alignItems: "center"}}>
-                There was an error with WebGL. See web console for more details.
-            </div>
-        );
-    };
-
     const routes = createRoutesFromElements(
         <Route path="/" element={<Root />} >
-            <Route index path="/" element={<Home />} errorElement={<ErrorBoundaryHome/>}/>
+            <Route index path="/" element={<Home />} errorElement={<Donut/>}/>
             <Route path="/about-me" element={<AboutMe />}/>
             <Route path="/blog" element={<BlogList />} loader={articleLoader}/>
             <Route path="/blog/:contentUrl/article" element={<BlogArticle />} loader={({ params }) => articleTextLoader(params.contentUrl)} />
--- personal/src/components/Home.tsx
+++ personal/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 
 import * as THREE from "three";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
@@ -26,18 +26,18 @@ const Background = () => {
         "resources/fantasy-islands-c.jpg",
         "resources/shrub-river-c.jpg",
         "resources/plateau-river-c.jpg",
-    ]
+    ];
 
     const bg = useLoader(TextureLoader, bg_array[Math.floor(Math.random() * bg_array.length)]);
 
-    const sphere_mesh = useRef<Mesh>(null!)
+    const sphere_mesh = useRef<Mesh>(null!);
+
+    let size = -8.3 * Math.pow(Math.log((window.innerWidth / 100) - 2), -1);
 
     useFrame(({ clock }) => {
         sphere_mesh.current.rotation.x = Math.sin(clock.getElapsedTime()) * 0.05;
-        sphere_mesh.current.rotation.y = clock.getElapsedTime() * 0.1;
-    })
-
-    let size = -8.3 * Math.pow(Math.log((window.innerWidth / 100) - 2), -1);
+        sphere_mesh.current.rotation.y = clock.getElapsedTime() * 0.05;
+    });
 
     return(
         <>
@@ -54,8 +54,69 @@ const Background = () => {
                 </mesh>
             </mesh>
         </>
+    );
+}
+
+export function Donut() {
+    const [ab, setAB] = useState<number[]>([1, 1]);
+    const donut = useRef<HTMLPreElement>(null!);
+
+    const asciiframe = async () => {
+        let nums : number[] = ab
+        nums[0] += 0.07;
+        nums[1] += 0.03;
+
+        let b : string[] = [];
+        let z = [];
+
+        const cA : number = Math.cos(nums[0]);
+        const sA : number = Math.sin(nums[0]);
+        const cB : number = Math.cos(nums[1]);
+        const sB : number = Math.sin(nums[1]);
+
+        for (let k = 0; k < 1760; k++) {
+            b[k] = k % 80 == 79 ? "\n" : " ";
+            z[k] = 0;
+        }
+
+        for (let j = 0; j < 6.28; j += 0.07) {
+            const ct : number = Math.cos(j);
+            const st : number = Math.sin(j);
+            for (let i = 0; i < 6.28; i += 0.02) {
+                const sp : number = Math.sin(i);
+                const cp : number = Math.cos(i);
+                const h : number = ct + 2; 
+                const t : number = sp * h * cA - st * sA; 
+
+                const D : number = 1 / (sp * h * sA + st * cA + 5); 
+                const x : number = 0 | (40 + 30 * D * (cp * h * cB - t * sB));
+                const y : number = 0 | (12 + 15 * D * (cp * h * sB + t * cB));
+                const o : number = x + 80 * y;
+                const N : number = 0 | (8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB));
+                if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+
+        if (donut !== null && donut.current.innerHTML !== null) donut.current.innerHTML = b.join("")
+        setAB(nums)
+    };
+
+    useEffect(() => {
+        if (donut)
+            setInterval(asciiframe, 100);
+    }, [donut]);
+
+    return (
+        <div className="container">
+            <span className="welcome">Welcome!</span>
+            <pre ref={donut} className="center">test</pre>
+        </div>
     )
 }
+
 
 export default function Home() {
     return(
